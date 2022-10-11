2599c0a9eafa1f25659cb01b2512ad1ebfbef34a
Danztee <olowoniyidaniel@gmail.com>
2022-10-11 12:39:43 +0100

    react-accordion

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
+
+function App() {
+  const questions = data;
+
+  return (
+    <div className="container">
+      <h3>questions and answers about login</h3>
+      <section className="info">
+        {questions.map((question) => (
+          <SingleQuestion key={question.id} {...question} />
+        ))}
+      </section>
+    </div>
+  );
+}
+
+export default App;
